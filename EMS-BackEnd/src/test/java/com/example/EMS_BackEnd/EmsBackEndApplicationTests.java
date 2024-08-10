e201792fb0808f071b554ef4f44738ff63e8fb25
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-08-10 09:00:16 +0530

    initial commit

---
+++ EMS-BackEnd/src/test/java/com/example/EMS_BackEnd/EmsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EMS_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
